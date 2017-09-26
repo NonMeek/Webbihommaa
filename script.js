daa0626ed1e1bdfd9fcc7820b4a45a5398ec0cb1
Stefan Gasic <stefan.gasic@gmail.com>
2017-09-26 19:33:33 +0300

    added toggleClass function

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ function showText(text) {
     let elem  = document.getElementById("teksti-kappale");
     elem.textContent = text;
 }
+
+function toggleClass(elemId, className) {
+    let elem = document.getElementById(elemId);
+    elem.classList.toggle(className);
+}
